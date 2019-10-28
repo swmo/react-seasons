876128efdcde701ee5b0eee50d92345c6a89d4a7
Moses Tschanz <moses.tschanz@gmail.com>
2019-10-28 11:23:14 +0100

    finish testing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
 class App extends React.Component {
 
     state = {lat: null, errorMessage: ''};
     
     componentDidMount(){
-        
-
         window.navigator.geolocation.getCurrentPosition(
             (position) => { //sucess callback if he figured out an position guess
                 this.setState({lat: position.coords.latitude})
@@ -21,24 +20,29 @@ class App extends React.Component {
     }
 
     componentDidUpdate(){
-        console.log('my component was updated - it rerendert');
+     //   console.log('my component was updated - it rerendert');
     }
 
-    render(){
-
+    renderContent(){
         if(this.state.lat){
-            return (
-                <div> 
-                    Latitude: {this.state.lat} 
-                </div>
-            )
+            return  <SeasonDisplay lat={this.state.lat} />
         }
 
         if(this.state.errorMessage){
             return <div>Error: {this.state.errorMessage}</div>
         }
 
-        return <div>loading..</div>
+        return (
+            <Loader text="please allow to know your location" />
+        )
+    }
+
+    render(){
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
